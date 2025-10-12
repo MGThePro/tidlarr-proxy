f264ea1d080b4d63df76783469daccf9a19b5faa
mgthepro <17652761+MGThePro@users.noreply.github.com>
2025-10-12 12:59:07 +0200

    add new api links, randomly distribute load across api, wait less

--- src/main.go
+++ src/main.go
@@ -15,7 +15,7 @@ import (
 var DownloadPath string
 var Category string
 var Port string
-var ApiLink = [...]string{"https://kraken.squid.wtf", "https://triton.squid.wtf", "https://zeus.squid.wtf", "https://aether.squid.wtf", "https://tidal-api-2.binimum.org", "https://tidal.401658.xyz"}
+var ApiLink = [...]string{"https://kraken.squid.wtf", "https://triton.squid.wtf", "https://zeus.squid.wtf", "https://aether.squid.wtf", "https://tidal-api-2.binimum.org", "https://tidal.401658.xyz", "https://hund.qqdl.site", "https://katze.qqdl.site", "https://maus.qqdl.site" , "https://vogel.qqdl.site", "https://wolf.qqdl.site"}
 var ApiKey string
 
 func getEnv(key string, fallback string) string {
@@ -84,8 +84,9 @@ func main() {
 }
 
 func request(query string) (string, error) {
-	for tries := 0; tries < 20; tries++ {
-		link := ApiLink[tries%len(ApiLink)]
+	var offset int = rand.Intn(len(ApiLink))
+	for tries := 0; tries < len(ApiLink)*3; tries++ {
+		link := ApiLink[(tries+offset) % len(ApiLink)]
 		fmt.Println("Trying URL " + link + query)
 		resp, err := http.Get(link + query)
 		if err != nil {
@@ -101,7 +102,7 @@ func request(query string) (string, error) {
 		if resp.Status == "200 OK" {
 			return string(bodyBytes), nil
 		}
-		duration, _ := time.ParseDuration(strconv.Itoa(1+tries) + "s")
+		duration, _ := time.ParseDuration(strconv.Itoa((tries / len(ApiLink))) + "s")
 		time.Sleep(duration)
 	}
 	return "", errors.New("Request failed, servers probably overloaded")
